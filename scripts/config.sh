37a263c0e678a908e176389babf2dae8b173c64f
Valentino Amato <105114984+valentinoamato@users.noreply.github.com>
2024-07-28 19:40:46 -0300

    Update config.sh to apply basic ufw configuration

--- scripts/config.sh
+++ scripts/config.sh
@@ -2,3 +2,9 @@
 
 #Set Zsh as default shell
 chsh -s /usr/bin/zsh
+
+#Apply basic UFW configuration https://wiki.archlinux.org/title/Uncomplicated_Firewall#Basic_configuration
+sudo ufw default deny
+sudo ufw allow from 192.168.0.0/24
+sudo ufw limit ssh
+sudo ufw enable
