26dbb8df9dc67e49c5b3f0562431f7d3b40bd6f9
Valentino Amato <105114984+valentinoamato@users.noreply.github.com>
2024-07-28 23:52:49 -0300

    Add config files for lightdm-gtk-greeter

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -2,3 +2,7 @@
 
 #Copy background to be used by lightdm greeter
 sudo cp ../media/backgrounds/arch-boolean.jpg /usr/share/pixmaps/arch-boolean.jpg
+
+#Copy lightdm-gtk-greeter.conf
+sudo mkdir -p /etc/lightdm/
+sudo cp ../config/system/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
