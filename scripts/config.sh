6b2b3d65c5908f07d398071246d2a57bf2db5af1
Valentino Amato <105114984+valentinoamato@users.noreply.github.com>
2024-07-29 20:33:21 -0300

    Update config for ohmyzsh

--- scripts/config.sh
+++ scripts/config.sh
@@ -3,6 +3,9 @@
 #Set Zsh as default shell
 chsh -s /usr/bin/zsh
 
+#Set Oh My Zsh theme
+zsh -c "source $HOME/.zshrc; omz theme set agnoster"
+
 #Apply basic UFW configuration https://wiki.archlinux.org/title/Uncomplicated_Firewall#Basic_configuration
 sudo ufw default deny
 sudo ufw allow from 192.168.0.0/24
