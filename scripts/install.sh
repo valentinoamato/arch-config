afc403cf45a2222b1ea0aaeca4579b3cb6127272
Valentino Amato <105114984+valentinoamato@users.noreply.github.com>
2024-07-31 19:16:30 -0300

    Fix ohmyzsh installation overwriting .zshrc

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,7 +11,12 @@ cd ..
 rm -rf fonts
 
 #Install ohmyzsh https://github.com/ohmyzsh/ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#Oh-my-zsh attacks .zshrc so i protect it
+mv $HOME/.zshrc $HOME/.zshrclink.bak
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+#Delete the .zshrc created by ohmyzsh and restore the original
+rm $HOME/.zshrc
+mv $HOME/.zshrclink.bak $HOME/.zshrc
 
 #Install pacman packages
 packages=$(cat ../packages/pacman/list.txt)
