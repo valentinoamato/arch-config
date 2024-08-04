1fbaa8d912cb1e6b76595ea5cae9f489846951e5
Valentino Amato <105114984+valentinoamato@users.noreply.github.com>
2024-08-04 17:18:04 -0300

    Update neovim config

--- config/user/.config/nvim/init.lua
+++ config/user/.config/nvim/init.lua
@@ -207,8 +207,32 @@ vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
 -- Setup gitsigns.nvim
-require("gitsigns").setup()
+local gitsigns = require("gitsigns")
+gitsigns.setup({
+    on_attach = function(bufnr)
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+        -- Navigation
+        map('n', ']c', function()
+          if vim.wo.diff then
+            vim.cmd.normal({']c', bang = true})
+          else
+            gitsigns.nav_hunk('next')
+          end
+        end)
 
+        map('n', '[c', function()
+          if vim.wo.diff then
+            vim.cmd.normal({'[c', bang = true})
+          else
+            gitsigns.nav_hunk('prev')
+          end
+        end)
+    end
+})
 -- Setup lualine.nvim
 require("lualine").setup()
 
