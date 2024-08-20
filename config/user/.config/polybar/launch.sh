43be27267fb34e597cde0fcc518515501a6fb9bd
Valentino Amato <105114984+valentinoamato@users.noreply.github.com>
2024-08-19 23:00:58 -0300

    Update polybar config

--- config/user/.config/polybar/launch.sh
+++ config/user/.config/polybar/launch.sh
@@ -5,12 +5,18 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
+#List batteries and adapters
+output=$(ls -1 /sys/class/power_supply 2>/dev/null)
+lines=($output)
+adapter="${lines[0]:-}"
+battery="${lines[1]:-}"
+
 # Launch Polybar in all monitors, using default config location ~/.config/polybar/config.ini
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar example 2>&1 | tee -a /tmp/polybar.log & disown
+    ADAPTER=$adapter BATTERY=$battery MONITOR=$m polybar example 2>&1 | tee -a /tmp/polybar.log & disown
   done
 else
-  polybar example 2>&1 | tee -a /tmp/polybar.log & disown
+  ADAPTER=$adapter BATTERY=$battery polybar example 2>&1 | tee -a /tmp/polybar.log & disown
 fi
 echo "Polybar launched..."
