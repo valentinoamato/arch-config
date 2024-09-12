b1d56cc88c5a7511fc50881f76597381f72bba22
Valentino Amato <105114984+valentinoamato@users.noreply.github.com>
2024-09-12 09:13:34 -0300

    Adjust window's label max length based on screen width

--- config/user/.config/polybar/launch.sh
+++ config/user/.config/polybar/launch.sh
@@ -11,10 +11,14 @@ lines=($output)
 adapter="${lines[0]:-}"
 battery="${lines[1]:-}"
 
+#Adjust the window's label length based on the screen width
+screen_width=$(xrandr --query | grep " connected" | cut -d" " -f4 | cut -d"x" -f1)
+title_length=$((screen_width / 40))
+
 # Launch Polybar in all monitors, using default config location ~/.config/polybar/config.ini
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    ADAPTER=$adapter BATTERY=$battery MONITOR=$m polybar example 2>&1 | tee -a /tmp/polybar.log & disown
+    TITLE_LENGTH=$title_length ADAPTER=$adapter BATTERY=$battery MONITOR=$m polybar example 2>&1 | tee -a /tmp/polybar.log & disown
   done
 else
   ADAPTER=$adapter BATTERY=$battery polybar example 2>&1 | tee -a /tmp/polybar.log & disown
