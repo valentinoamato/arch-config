8311375d5526edc9039ebfe7a40aca3b63a7da90
Valentino Amato <105114984+valentinoamato@users.noreply.github.com>
2024-09-13 10:08:22 -0300

    Update polybar config

--- config/user/.config/polybar/launch.sh
+++ config/user/.config/polybar/launch.sh
@@ -11,13 +11,14 @@ lines=($output)
 adapter="${lines[0]:-}"
 battery="${lines[1]:-}"
 
-#Adjust the window's label length based on the screen width
-screen_width=$(xrandr --query | grep " connected" | cut -d" " -f4 | cut -d"x" -f1)
-title_length=$((screen_width / 40))
-
 # Launch Polybar in all monitors, using default config location ~/.config/polybar/config.ini
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+
+      #Adjust the window's label length based on the screen width
+      screen_width=$(xrandr --query | grep $m | grep -Po '[0-9]{4}(?=x)')
+      title_length=$((screen_width / 23))
+
     TITLE_LENGTH=$title_length ADAPTER=$adapter BATTERY=$battery MONITOR=$m polybar example 2>&1 | tee -a /tmp/polybar.log & disown
   done
 else
